0afa7750f17b2b640696af9684daeacec6807d33
dtieso <dtieso@walit.it>
2024-03-19 23:18:57 +0100

    wip:simple parallax

--- assets/js/animationHandler.js
+++ assets/js/animationHandler.js
@@ -82,97 +82,55 @@ export function initScroll(){
 
     const scroll = new ScrollAnimations();
     window.w.scroll = scroll;
+        
+    // const bgTween = new TimelineMax()
+    //     .to("#PinContainer #BackgroundContainer",1,{left: "-400%", ease: Linear.easeNone} )
+
+    // const scene = new ScrollMagic.Scene(
+    //     { triggerElement: "#PinContainer", duration: 200, offset: 450 })
+    //     .setTween(bgTween)
+    //     .setPin("#PinContainer")
+    //     .addIndicators()
+    //     .addTo(scroll.getController());
+    // // Tweens definitions
+    // const bgTween = new TimelineMax()
+    //     .to("#SliderContainer", 1, {x: "-25%"})
+  	// 	.to("#SliderContainer", 1, {x: "-50%"})
+  	// 	.to("#SliderContainer", 1, {x: "-75%"})      
     
-    // Tweens definitions
-    const horizontalScrollTween = new TimelineMax()
-        .to("#SliderContainer", 1, {x: "-25%"})
-  		.to("#SliderContainer", 1, {x: "-50%"})
-  		.to("#SliderContainer", 1, {x: "-75%"})      
-
-    const sunTween = new TimelineMax()
-        .fromTo("#sun",1,{left:0,top:"20%"},{left:"20%",top:"-10%"})
-        .to("#sun",1,{left:"70%",top:"6.875%"})  
-
-    const slide1Parallax = new TimelineMax()
-        .add([
-            TweenMax.fromTo("#ParallaxOne .layer-fast", 1, 
-                {left: "25%",opacity:2},{left:"-25%",opacity:0,ease: "linear"}
-            ),
-            TweenMax.fromTo("#ParallaxOne .layer-slow",1,
-                {left:"-25%",opacity:2},{left:"-50%",opacity:0,ease: "linear"}
-            )
-        ]);
-    const slide2Parallax = new TimelineMax()
-        .add([
-            TweenMax.fromTo("#ParallaxTwo .layer-fast",1,
-                {left: "25%",opacity:2},{left:"-25%",opacity:0,ease: "linear"}
-            ),
-            TweenMax.fromTo("#ParallaxTwo .layer-slow",1,
-                {left:"-25%",opacity:2},{left:"-50%",opacity:0,ease: "linear"}
-            ),
-        ])
-
-    const slide3Parallax = new TimelineMax()
-        .add([
-            TweenMax.fromTo("#ParallaxThree .layer-fast",1,
-                {left: "25%",opacity:2},{left:"-25%",opacity:0,ease: "linear"}
-            ),
-            TweenMax.fromTo("#ParallaxThree .layer-slow",1,
-                {left:"-25%",opacity:2},{left:"-50%",opacity:0,ease: "linear"}
-            ),
-        ])
-
-    const slide4Parallax = new TimelineMax()
-        .add([
-            TweenMax.fromTo("#ParallaxFour .layer-fast",1,
-                {left: "100%",opacity:0},{left:0,opacity:1,ease: "linear"}
-            ),
-            TweenMax.fromTo("#ParallaxFour .layer-slow",1,
-                {left:"100",opacity:0},{left:"-40%",opacity:1,ease: "linear"}
-            ),
-        ])
-;
-
-    // Main Horizontal Tween and Scene
-    scroll.setTween(horizontalScrollTween,"mainHorizontalTween","#SliderContainer");
-    scroll.createScene({
-        triggerElement:"#PinContainer",
-        triggerHook:0,
-        duration:"400%"
-    },"#PinContainer",horizontalScrollTween,"mainHorizontalScene")
+    // const sceneTween = new TimelineMax()
+    //     .to("#ScenesContainer", 1, {x: "-25%"})
+  	// 	.to("#ScenesContainer", 1, {x: "-50%"})
+  	// 	.to("#ScenesContainer", 1, {x: "-75%"})      
     
-    // Sun Tween and Scene
-    scroll.setTween(sunTween,"sunTween","#sun");
-    scroll.createScene({
-      triggerElement:"#PinContainer",
-      duration:"400%"  
-    },null,sunTween,"sunScene")
+    // const topTween = new TimelineMax()
+    //     .to("#TopContainer", 1, {x: "-25%"})
+  	// 	.to("#TopContainer", 1, {x: "-50%"})
+  	// 	.to("#TopContainer", 1, {x: "-75%"})      
 
-    // Parallax Tween and Scene
-    scroll.setTween(slide1Parallax,"parallaxOne",["#ParallaxOne .layer-back","#ParallaxOne .layer-base"]);
-    scroll.createScene({
-        triggerElement:"#PinContainer",
-        duration:"100%",
-    },null,slide1Parallax,"parallax")
 
-    scroll.setTween(slide2Parallax,"parallaxTwo",["#ParallaxTwo .layer-back","#ParallaxTwo .layer-base"]);
-    scroll.createScene({
-        triggerElement:"#PinContainer",
-        duration:"300%",
-        offset:"300%"
-    },null,slide2Parallax,"parallax")
 
-    scroll.setTween(slide1Parallax,"parallaxThree",["#ParallaxThree .layer-back","#ParallaxThree .layer-base"]);
-    scroll.createScene({
-        triggerElement:"#PinContainer",
-        duration:"400%",
-        offset:"900%"
-    },null,slide3Parallax,"parallax")
-
-    scroll.setTween(slide2Parallax,"parallaxFour",["#ParallaxFour .layer-back","#ParallaxFour .layer-base"]);
-    scroll.createScene({
-        triggerElement:"#PinContainer",
-        duration:"400%",
-        offset:"400%"
-    },null,slide4Parallax,"parallax",true)
+    // // Main Horizontal Tween and Scene
+    // scroll.setTween(bgTween,"bgTween","#SliderContainer");
+    // scroll.createScene({
+    //     triggerElement:"#PinContainer",
+    //     triggerHook:0,
+    //     duration:"400%"
+    // },"#PinContainer",bgTween,"mainHorizontalScene")
+    
+    // scroll.setTween(bgTween,"scenesTween","#ScenesContainer");
+    // scroll.createScene({
+    //     triggerElement:"#PinContainer",
+    //     triggerHook:0,
+    //     duration:"400%"
+    // },"#PinContainer",bgTween,"scenesTween")
+    
+    // scroll.setTween(bgTween,"topTween","#TopContainer");
+    // scroll.createScene({
+    //     triggerElement:"#PinContainer",
+    //     triggerHook:0,
+    //     duration:"400%"
+    // },"#PinContainer",bgTween,"topTween")
+    
+    
 }
